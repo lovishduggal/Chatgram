aef200569a0c188a178118cc2735f33d0f0120ec
Lovish Duggal <lovishduggal11@gmail.com>
2024-02-03 14:59:31 +0530

    Implemented  getAllComments, updateComment and deleteComment routes

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -4,8 +4,8 @@ import { Post } from '../models/post.model.js';
 import ErrorHandler from '../utils/customErrorClass.js';
 
 const handleCreateComment = catchAsyncError(async (req, res, next) => {
-    const postId = req.params.id;
-    const userId = req.user._id;
+    const postId = req.params.id; // Get ID of the post
+    const userId = req.user._id; // Get ID of current user2222
     const { content } = req.body;
 
     const existingPost = await Post.findById(postId);
@@ -26,4 +26,60 @@ const handleCreateComment = catchAsyncError(async (req, res, next) => {
         newComment,
     });
 });
-export { handleCreateComment };
+
+const handleGetAllComments = catchAsyncError(async (req, res) => {
+    const postId = req.params.id; // Get ID of the post
+    const comments = await Comment.find({ post: postId }).populate('user'); // Populate all info of user
+    return res.status(200).json({
+        success: true,
+        comments,
+    });
+});
+
+const handleUpdateComment = catchAsyncError(async (req, res, next) => {
+    const postId = req.params.id; // Get ID of the post
+    const commentId = req.params.commentId; // Get ID of the comment
+    const { content } = req.body;
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        { content },
+        { new: true }
+    );
+    if (!updatedComment || !(updatedComment.post.toString() !== postId)) {
+        // Ensure comment belongs to post
+        return next(new ErrorHandler('Comment not found', 404));
+    }
+    return res.status(200).json({
+        success: true,
+        updatedComment,
+    });
+});
+
+const handleDeleteComment = catchAsyncError(async (req, res, next) => {
+    const postId = req.params.postId; // Get ID of the post
+    const commentId = req.params.commentId; // Get ID of the comment
+    const comment = await Comment.findById(commentId);
+    console.log(comment, commentId, postId);
+    console.log(!comment || comment.post.toString() !== postId);
+    if (!comment || comment.post.toString() !== postId) {
+        // Ensure comment belongs to post
+        return next(new ErrorHandler('Comment not found', 404));
+    }
+
+    await comment.deleteOne();
+
+    await Post.findByIdAndUpdate(postId, {
+        $pull: { comments: commentId }, // Remove comment ID from post's comments array
+    });
+
+    return res.status(200).json({
+        success: true,
+        comment,
+    });
+});
+export {
+    handleCreateComment,
+    handleGetAllComments,
+    handleUpdateComment,
+    handleDeleteComment,
+};
--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,6 +1,14 @@
 import { Router } from 'express';
-import { handleCreateComment } from '../controllers/comment.controller.js';
+import {
+    handleCreateComment,
+    handleDeleteComment,
+    handleGetAllComments,
+    handleUpdateComment,
+} from '../controllers/comment.controller.js';
 const router = Router();
 
+router.get('/:id', handleGetAllComments);
 router.post('/:id', handleCreateComment);
+router.put('/:postId/comments/:commentId', handleUpdateComment);
+router.delete('/:postId/comments/:commentId', handleDeleteComment);
 export default router;
