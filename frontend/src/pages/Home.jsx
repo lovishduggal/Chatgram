13dafdf6ed0b07478ba982b8d43c03fb8da632b2
Lovish Duggal <lovishduggal11@gmail.com>
2024-02-13 13:36:32 +0530

    Reconstructed the NavBar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,6 @@
-import { Grid } from '@mui/material';
-import TopBar from '../features/stream/components/nav/TopBar';
-import Navigation from '../features/stream/components/nav/Navigation';
-import Feed from '../features/stream/components/feed/Feed';
-import LeftNavigation from '../features/stream/components/nav/LeftNavigation';
-
+import NavBar from '../features/nav/NavBar';
 function Home() {
-    return (
-        <Grid container>
-            <TopBar></TopBar>
-            <LeftNavigation></LeftNavigation>
-            <Feed></Feed>
-            <Navigation></Navigation>
-        </Grid>
-    );
+    return <NavBar></NavBar>;
 }
 
 export default Home;
